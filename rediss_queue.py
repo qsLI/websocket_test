d75549710c89e49add6b791e525871f18c2d8163
KL <liqisheng@whu.edu.cn>
2015-12-17 13:20:02 +0800

    refactor some method into this class

--- rediss_queue.py
+++ rediss_queue.py
@@ -1,11 +1,18 @@
 # coding=utf-8
 import redis
+from StringIO import StringIO
+import numpy as np
+import cv2
+
+
 class RedisQueue(object):
     def __init__(self, name, namespace='queue', **redis_kwargs):
         self.__db = redis.StrictRedis()
         self.key = '%s:%s' % (namespace, name)
+
     def put(self, item):
         self.__db.rpush(self.key, item)
+
     def get(self, type=True, timeout=None):
         if type:
             item =  self.__db.blpop(self.key, timeout)
@@ -15,8 +22,25 @@ class RedisQueue(object):
             # print item
             item = item[1]
         return item
+
     def size(self):
         return self.__db.llen(self.key)
+
+    def decode_one_frame(self):
+        """ decode a frame from message queue """
+        img = self.get()
+        sio = StringIO(img)
+        img = np.load(sio)
+        img = cv2.imdecode(img, cv2.CV_LOAD_IMAGE_COLOR)
+        return img
+
+    def encode_one_frame(self, img):
+        """ encode a frame to message queue"""
+        ret, img = cv2.imencode(".jpg", img)
+        sio = StringIO()
+        np.save(sio, img)
+        self.put(sio.getvalue())
+
 
 if __name__ == "__main__":
     q = RedisQueue('test')
